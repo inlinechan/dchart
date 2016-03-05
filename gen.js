8de9d53b0e75deef5e154e9bcfcba0101b746dce
Hyungchan Kim <inlinechan@gmail.com>
2016-03-05 23:42:47 +0900

    Split y data range

--- gen.js
+++ gen.js
@@ -12,7 +12,7 @@ if (process.argv.length > 3)
 setTimeout(function() {
     setInterval(function() {
         var value = ymin + (Math.random() * (ymax - ymin));
-        var cost = ymin + (Math.random() * (ymax - ymin));
+        var cost = 100 + (Math.random() * (600 - 100));
         console.log(id + ' ' + parseInt(value) + ' ' + parseInt(cost));
         id += 1;
     }, interval);
